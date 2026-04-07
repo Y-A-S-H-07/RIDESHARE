4ceb44a7716591a78f36a620f7a350a4f7128cda
Y-A-S-H-07 <yashdabhekar007@gmail.com>
2026-04-07 14:52:44 +0530

    fix api

--- rideshare-backend/src/main/java/com/rideshare/rideshare/config/CorsConfig.java
+++ rideshare-backend/src/main/java/com/rideshare/rideshare/config/CorsConfig.java
@@ -2,7 +2,8 @@ package com.rideshare.rideshare.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
@@ -13,8 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // frontend
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("https://rideshare-x9qt.onrender.com")   // allow all (for now)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
         };
